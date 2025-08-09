b093be1b8cb5b0c9cd5e903c2ea4bfab8c6c7101
minjeeki <minjeeki@student.42seoul.kr>
2025-08-09 13:11:09 +0900

    modify: delete import of App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import {QueryProvider} from "@/providers/QueryProvider";
 import {RouterProvider} from "react-router";
 import {router} from "@/lib/router";
 
-import "./App.css";
-
 const App = () => {
   return (
     <QueryProvider>
